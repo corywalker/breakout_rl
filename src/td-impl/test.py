bb1926a8c4239547209c153178c9b1d2bad2bb90
Benjamin Brock <brock@utk.edu>
2015-11-23 20:08:16 -0500

    Almost finished

--- src/td-impl/test.py
+++ src/td-impl/test.py
@@ -4,107 +4,136 @@ from ale_python_interface import ALEInterface
 import numpy as np
 from PIL import Image
 
-def get_paddle_pos(screen):
-    xStart = -1
-    xEnd = -1
-    for pos in range(8, 152):
-        (r, g, b) = screen[189, pos]
-        if (r == 200 and g == 72 and b == 72) or (r == 184 and g == 50 and b == 50):
-            xStart = pos
-            break
-    for pos in range(xStart, 152):
-        (r, g, b) = screen[189, pos]
-        xEnd = pos
-        if ~((r == 200 and g == 72 and b == 72) or (r == 184 and g == 50 and b == 50)):
-            break
+class Game:
+    def __init__(self, alpha, gamma, epsilon):
+        # State values, intiialized arbitrarily
+        self.value = np.random.rand(2, 8)
 
-    paddleX = (xEnd + xStart)/2
-    return (paddleX, 189)
+        # Hyperparameters
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
 
-def get_ball_pos(screen):
-    ballX = -1
-    ballY = -1
+        # Initialize ALE
+        self.ale = ALEInterface()
 
-    for x in range(8, 151):
-        for y in range(50, 189):
-            (r, g, b) = screen[y, x]
-            if(r == 184 and g == 50 and b == 50):
-                ballX = x+1
-                ballY = y+2
+        self.ballX = self.ballY = -1
+        self.paddleX = self.paddleY = -1
+
+    # Run an episode
+    def run_episode(self):
+        self.ale.loadROM('breakout.bin')
+        self.legal_actions = ale.getLegalActionSet()
+
+        i = 0
+        noBall = 0
+        gameOver = False
+        state = newstate = -1
+        action = newaction = -1
+
+        # Fire to start the game
+        self.ale.act(legal_actions[1])
+        self.ale.act(legal_actions[1])
+
+        while not gameOver
+            screen = ale.getScreenRGB()
+
+            # Save image for examination
+            image = Image.fromarray(screen)
+            image.save("images/" + str(i) + ".png", "png")
+            i += 1
+
+            self.ballX, self.ballY = self.get_ball_pos(screen)
+            self.paddleX, self.paddleY = self.get_paddle_pos(screen)
+
+            # Color in screen for ball position
+            screen[self.ballY, self.ballX] = (255, 255, 255)
+            screen[self.paddleY, self.paddleX] = (255, 255, 255)
+
+            newstate = get_state(self.ballX - self.paddleX)
+
+            # Whoops, you died.
+            if (noBall > 60):
+                reward = -10
+                newstate = 7
+                self.ale.act(legal_actions[1])
+                noBall = 0
+
+                self.values[action][state] = self.values[action][state] + self.alpha * (reward + self.gamma * )
+
+                if self.ale.game_over():
+                    gameOver = True
+                continue
+
+            # Ignore this timestep; can't see ball.
+            if (ballX == -1 or ballY == -1):
+                noBall += 1
+                ale.act(legal_actions[0])
+                continue
+            else:
+                noBall = 0
+
+            # Paddle is out of game area. Ignore timestep and readjust.
+            if (paddleX >= 138):
+                move_left(ale, legal_actions)
+
+    def get_paddle_pos(self, screen):
+        xStart = -1
+        xEnd = -1
+        for pos in range(8, 152):
+            (r, g, b) = screen[189, pos]
+            if (r == 200 and g == 72 and b == 72) or (r == 184 and g == 50 and b == 50):
+                xStart = pos
                 break
-    return (ballX, ballY)
+        for pos in range(xStart, 152):
+            (r, g, b) = screen[189, pos]
+            xEnd = pos
+            if ~((r == 200 and g == 72 and b == 72) or (r == 184 and g == 50 and b == 50)):
+                break
 
-def move_left(ale, legal_actions):
-    return ale.act(legal_actions[4])
-def move_right(ale, legal_actions):
-    return ale.act(legal_actions[3])
+        self.paddleX = (xEnd + xStart)/2
+        self.paddleY = 189
+        return (self.paddleX, 189)
 
+    def get_ball_pos(self, screen):
+        self.ballX = -1
+        self.ballY = -1
 
-ale = ALEInterface()
+        for x in range(8, 151):
+            for y in range(50, 189):
+                (r, g, b) = screen[y, x]
+                if(r == 184 and g == 50 and b == 50):
+                    self.ballX = x+1
+                    self.ballY = y+2
+                    break
+        return (self.ballX, self.ballY)
 
+    def move_left(self,):
+        self.ale.act(self.legal_actions[4])
+        return 0
+    def move_right(self,):
+        self.ale.act(self.legal_actions[3])
+        return 1
 
-# Set USE_SDL to true to display the screen. ALE must be compilied
-# with SDL enabled for this to work. On OSX, pygame init is used to
-# proxy-call SDL_main.
-USE_SDL = False
-if USE_SDL:
-    if sys.platform == 'darwin':
-        import pygame
-        pygame.init()
-        ale.setBool('sound', False) # Sound doesn't work on OSX
-    elif sys.platform.startswith('linux'):
-        ale.setBool('sound', True)
-    ale.setBool('display_screen', True)
+    def get_state(self, dist):
+        if dist < -7:
+            return 0
+        if dist in [-7, -6, -5]:
+            return 1
+        if dist in [-4, -3, -2]:
+            return 2
+        if dist in [-1, 0, 1]:
+            return 3
+        if dist in [2, 3, 4]:
+            return 4
+        if dist in [5, 6, 7]:
+            return 5
+        return 6
 
-# Load the ROM file
-ale.loadROM('breakout.bin')
+    def get_best_action(self, state):
+        return max(self.values[0][state], self.values[1][state])
 
-# Get the list of legal actions
-legal_actions = ale.getLegalActionSet()
-
-screen_width, screen_height = ale.getScreenDims()
-
-i = 0
-noBall = 0
-
-ale.act(legal_actions[1])
-ale.act(legal_actions[1])
-
-while not ale.game_over():
-    screen = ale.getScreenRGB()
-
-    ballX, ballY = get_ball_pos(screen)
-    paddleX, paddleY = get_paddle_pos(screen)
-
-    screen[ballY, ballX] = (255, 255, 255)
-    screen[paddleY, paddleX] = (255, 255, 255)
-
-    if (not (ballX == -1 or ballY == -1)):
-        print str(ballX) + "," + str(ballY)
-    if (not (paddleX == -1 or paddleY == -1)):
-        print str(paddleX) + "," + str(paddleY)
-
-    image = Image.fromarray(screen)
-    image.save("images/" + str(i) + ".png", "png")
-    i += 1
-
-    if (noBall > 60):
-        ale.act(legal_actions[1])
-        noBall = 0
-        continue
-
-    if (ballX == -1 or ballY == -1):
-        noBall += 1
-        ale.act(legal_actions[0])
-        continue
-    else:
-        noBall = 0
-
-    if (paddleX >= 138):
-        move_left(ale, legal_actions)
-    elif (ballX - paddleX > 5):
-        move_right(ale, legal_actions)
-    elif (ballX - paddleX < -5):
-        move_left(ale, legal_actions)
-    else:
-        ale.act(legal_actions[0])
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return self.values[np.random.randint(2)][state]
+        return get_best_action(state)
